98aac2fcb1388195c9951410f55b70f12df45843
keffri <keffri.w.neal@gmail.com>
2022-05-31 18:52:30 -0700

    basic menu layout

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Menu = () => {
+const Menu = (props) => {
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className="menu">
+      <h1 className="menu__title">Find these characters!</h1>
+      <li className="menu__list">
+        <ol>easy - alex mercer</ol>
+        <ol>medium - predator</ol>
+        <ol>hard - hell boy</ol>
+        <ol>hardest - sonic</ol>
+      </li>
+      <button onClick={props.startGame}>Play!</button>
     </div>
   );
 };
