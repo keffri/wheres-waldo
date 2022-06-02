85a989ee78a59bdc6b9265aae7802f6935e12c44
keffri <keffri.w.neal@gmail.com>
2022-06-01 19:05:42 -0700

    timer start

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const { time, setTime } = useState({ hr: 0, min: 0, sec: 0 });
+
   return (
     <header className="header">
       <h1 className="header__title">findr</h1>
