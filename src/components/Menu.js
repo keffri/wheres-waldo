56d658b3750b165f6ae218a9d1591923abef6971
keffri <keffri.w.neal@gmail.com>
2022-06-01 19:06:19 -0700

    images and classNames added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,30 @@
 import React from "react";
+import alexmercer from "../images/alexmercer.jpg";
+import predator from "../images/predator.jpg";
+import spiderman from "../images/spiderman.jpg";
+import sonic from "../images/sonic.jpg";
 
 const Menu = (props) => {
   return (
     <div className="menu">
-      <h1 className="menu__title">Find these characters!</h1>
+      <h1 className="menu__title">FIND THESE CHARACTERS!</h1>
       <li className="menu__list">
-        <ol>easy - alex mercer</ol>
-        <ol>medium - predator</ol>
-        <ol>hard - hell boy</ol>
-        <ol>hardest - sonic</ol>
+        <ol className="menu__item">
+          <img className="menu__image" src={alexmercer} alt="alexmercer" />
+          <p className="menu__item--text">Easy - Alex Mercer</p>
+        </ol>
+        <ol className="menu__item">
+          <img className="menu__image" src={predator} alt="predator" />
+          <p className="menu__item--text">Medium - Predator</p>
+        </ol>
+        <ol className="menu__item">
+          <img className="menu__image" src={spiderman} alt="s[iderman" />
+          <p className="menu__item--text">Hard - Spider-Man</p>
+        </ol>
+        <ol className="menu__item">
+          <img className="menu__image" src={sonic} alt="sonic" />
+          <p className="menu__item--text">Hardest - Sonic</p>
+        </ol>
       </li>
       <button onClick={props.startGame}>Play!</button>
     </div>
