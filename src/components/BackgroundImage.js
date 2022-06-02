8c0a54e26463cf9b64a7f94e25b6bda0fa93ece9
keffri <keffri.w.neal@gmail.com>
2022-06-01 19:05:25 -0700

    style change on state change

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,10 +1,18 @@
 import React from "react";
 import findrimg from "../images/egorklyuchnyk.jpg";
 
-const BackgroundImage = () => {
+const BackgroundImage = (props) => {
   return (
     <div className="backgroundImage">
-      <img className="findr" src={findrimg} alt="test" />
+      <img
+        style={{
+          filter: !props.playingState ? "brightness(30%)" : "brightness(100%)",
+          pointerEvents: !props.playingState ? "none" : "auto",
+        }}
+        className="findr"
+        src={findrimg}
+        alt="test"
+      />
     </div>
   );
 };
