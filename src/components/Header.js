13d92a6a409c42081b60e8f723e0eaf1df2ae6a3
keffri <keffri.w.neal@gmail.com>
2022-06-06 19:06:23 -0700

    showCharacterMenu functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,23 @@ import React from "react";
 import Stopwatch from "./Stopwatch";
 
 const Header = (props) => {
+  let button;
+  if (props.playingState) {
+    if (!props.showCharacters) {
+      button = (
+        <button onClick={props.showCharactersList}>Show Characters</button>
+      );
+    } else {
+      button = (
+        <button onClick={props.hideCharactersList}>Hide Characters</button>
+      );
+    }
+  }
+
   return (
     <header className="header">
       <h1 className="header__title">findr</h1>
+      {button}
       <Stopwatch playing={props.playingState} />
     </header>
   );
