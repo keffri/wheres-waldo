4664113a5106587fd8b73b0a41d8b85d1c9cea3c
keffri <keffri.w.neal@gmail.com>
2022-06-07 18:34:21 -0700

    CharacterPopup on click functionality

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import findrimg from "../images/egorklyuchnyk.jpg";
+import CharacterPopup from "./CharacterPopup";
 
 const BackgroundImage = (props) => {
+  const [mouseCoords, setMouseCoords] = useState({ x: 0, y: 0 });
+  const [popupCoords, setPopupCords] = useState({ x: 0, y: 0 });
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleMouseMove = (e) => {
+    setMouseCoords({
+      x: e.pageX,
+      y: e.pageY,
+    });
+  };
+
+  const showCharacterPopup = () => {
+    setPopupCords({ x: mouseCoords.x, y: mouseCoords.y });
+    setShowPopup(true);
+  };
+
   return (
     <div className="backgroundImage">
       <img
@@ -12,7 +29,10 @@ const BackgroundImage = (props) => {
         className="findr"
         src={findrimg}
         alt="test"
+        onMouseMove={handleMouseMove}
+        onClick={showCharacterPopup}
       />
+      {showPopup && <CharacterPopup popupCoords={popupCoords} />}
     </div>
   );
 };
