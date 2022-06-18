fbc6e91a10daece853c5c244ad056b818bcbbbc9
keffri <keffri.w.neal@gmail.com>
2022-06-18 12:27:10 -0700

    basic character search functionality

--- src/components/CharacterPopup.js
+++ src/components/CharacterPopup.js
@@ -2,8 +2,31 @@ import React from "react";
 
 const CharacterPopup = (props) => {
   const findCharacter = (name) => {
-    props.setCharacters(props.characters.filter((char) => char.name !== name));
-    props.hideCharacterPopup();
+    let mouseX = props.mouseCoords.x;
+    let mouseY = props.mouseCoords.y;
+
+    let [selectedCharacter] = props.characters.filter((char) => {
+      return char.name === name;
+    });
+    let removeCharArray = props.characters.filter((char) => {
+      return char.name !== name;
+    });
+
+    // console.log(removeCharArray);
+    if (
+      mouseX >= selectedCharacter.coords[0].startX &&
+      mouseX <= selectedCharacter.coords[1].endX &&
+      mouseY >= selectedCharacter.coords[0].startY &&
+      mouseY <= selectedCharacter.coords[1].endY
+    ) {
+      selectedCharacter.found = true;
+      alert(`You found ${selectedCharacter.name}!`);
+      props.setCharacters([...removeCharArray]);
+      props.hideCharacterPopup();
+    } else {
+      alert("Keep searching!");
+      props.hideCharacterPopup();
+    }
   };
 
   return (
