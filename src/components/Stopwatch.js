234746eb0fb4675a7bf44f153e196ebf4ca74b09
keffri <keffri.w.neal@gmail.com>
2022-06-20 18:10:52 -0700

    removed time state

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -8,7 +8,7 @@ const Stopwatch = (props) => {
 
     if (props.playing) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 10);
+        props.setTime((prevTime) => prevTime + 10);
       }, 10);
     } else {
       clearInterval(interval);
@@ -19,9 +19,9 @@ const Stopwatch = (props) => {
 
   return (
     <div>
-      <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)} : </span>
-      <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)} : </span>
-      <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      <span>{("0" + Math.floor((props.time / 60000) % 60)).slice(-2)} : </span>
+      <span>{("0" + Math.floor((props.time / 1000) % 60)).slice(-2)} : </span>
+      <span>{("0" + ((props.time / 10) % 100)).slice(-2)}</span>
     </div>
   );
 };
