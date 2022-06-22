ff891e2a182f1022efa3a3a2206a9449730ec73b
keffri <keffri.w.neal@gmail.com>
2022-06-21 18:48:22 -0700

    basic results screen

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Results = () => {
-  return <div className="results"></div>;
+const Results = (props) => {
+  return (
+    <div className="results">
+      <p>Congratulations, you found all the characters!</p>
+      <p>
+        Your time was:{" "}
+        <span>
+          {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)} :{" "}
+        </span>
+        <span>{("0" + Math.floor((props.time / 1000) % 60)).slice(-2)} : </span>
+        <span>{("0" + ((props.time / 10) % 100)).slice(-2)}</span>
+      </p>
+    </div>
+  );
 };
 
 export default Results;
