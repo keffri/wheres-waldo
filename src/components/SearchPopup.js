8decd834d8cb515a0ad5120ea353d0ca7f9932a6
keffri <keffri.w.neal@gmail.com>
2022-06-27 12:46:14 -0700

    SearchPopup component completion

--- src/components/SearchPopup.js
+++ src/components/SearchPopup.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const SearchPopup = () => {
-  return <div className="searchPopup"></div>;
+const SearchPopup = (props) => {
+  return (
+    <div
+      className="searchPopup"
+      style={{
+        backgroundColor: props.color,
+      }}
+    >
+      {props.character ? (
+        <p>You found {props.character}!</p>
+      ) : (
+        <p>Keep searching!</p>
+      )}
+    </div>
+  );
 };
 
 export default SearchPopup;
