95e4aac58862021f26db035f3a6b25b3ada7d207
keffri <keffri.w.neal@gmail.com>
2022-07-05 20:51:24 -0700

    removed coords from characters state

--- src/App.js
+++ src/App.js
@@ -12,34 +12,18 @@ function App() {
     {
       name: "Alex Mercer",
       found: false,
-      coords: [
-        { startX: 1690, startY: 1020 },
-        { endX: 1795, endY: 1175 },
-      ],
     },
     {
       name: "Predator",
       found: false,
-      coords: [
-        { startX: 1750, startY: 5340 },
-        { endX: 1835, endY: 5470 },
-      ],
     },
     {
       name: "Spider-Man",
       found: false,
-      coords: [
-        { startX: 1180, startY: 7455 },
-        { endX: 1270, endY: 7525 },
-      ],
     },
     {
       name: "Sonic",
       found: false,
-      coords: [
-        { startX: 620, startY: 6375 },
-        { endX: 675, endY: 6420 },
-      ],
     },
   ]);
 
